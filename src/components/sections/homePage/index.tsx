a8d9a497d88be43ecb65b5679c178ef77c7dbbcc
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-11 03:04:15 +0000

    Basic Project setup

---
+++ src/components/sections/homePage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SkillsHolder from './skills/skillsHolder'
+import MainBaseContent from './baseContent/mainBaseContent'
+
+function HomePage() {
+  return (
+    <div>
+       <MainBaseContent/>
+       <SkillsHolder/>
+    </div>
+  )
+}
+
+export default HomePage
