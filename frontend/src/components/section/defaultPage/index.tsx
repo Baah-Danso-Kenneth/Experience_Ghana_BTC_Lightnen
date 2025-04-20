10247e5dfcd94397b9baabbfea2141ecc4802bc0
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-20 13:51:50 +0000

    baseUI

--- frontend/src/components/section/defaultPage/index.tsx
+++ frontend/src/components/section/defaultPage/index.tsx
@@ -2,11 +2,13 @@ import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
 import React from 'react'
 import HeroSection from './heroSection'
+import BaseContent from './baseContent'
 
 function DefaultPage() {
   return (
     <div>
        <HeroSection/>
+       <BaseContent/>
       <Footer/>
     </div>
   )
