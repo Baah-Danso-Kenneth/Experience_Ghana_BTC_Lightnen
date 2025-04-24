3f86e05d48fe3b48311e72a827dbb612e37e04e0
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-24 12:18:30 +0000

    Dummy data shops

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -6,6 +6,31 @@ export const navLinks=[
     {name:'events',href:'/contacts'},
     {name:'donations',href:'/contacts'},
     {name:'festivals',href:'/contacts'},
+]
 
+export const shopItems=[
+    {name:'all', query:'#'},
+    {name:'accesssories', query:'#'},
+    {name:'apparel', query:'#'},
+    {name:'face masks', query:'#'},
+    {name:'gift cards', query:'#'},
+    {name:'hats', query:'#'},
+    {name:'toes', query:'#'},
+    {name:'travel guide', query:'#'},
+    {name:'shoes', query:'#'},
+    {name:'bags', query:'#'},
+]
+
+
+export const shopItemsContent=[
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
+    {image:'/images/sunset.jpg', title:'shoe-bag', description:'ghanaian leather made bag -guide', price:3200.00, category:'bags'},
 
 ]
