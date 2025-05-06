56b0229c89d3ca4c29236470d042d325107433b2
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-06 22:39:47 +0000

    Ammend useShop

--- frontend/src/hooks/useShop.ts
+++ frontend/src/hooks/useShop.ts
@@ -3,16 +3,21 @@
 
 import { getAllItems } from "@/redux/features/slices/shopSlice";
 import { AppDispatch, RootState } from "@/redux/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-export function useShop(){
+export function useShop() {
     const dispatch = useDispatch<AppDispatch>();
-    const {products} = useSelector((state:RootState)=>state.shopItems)
-
-    useEffect(()=>{
-        dispatch(getAllItems())
-    },[dispatch])
-
-    return {products}
-}
+    const { products, loading, error } = useSelector((state: RootState) => state.shopItems);
+    const [isInitialized, setIsInitialized] = useState(false);
+  
+    useEffect(() => {
+    
+      if (!products.length && !isInitialized) {
+        setIsInitialized(true);
+        dispatch(getAllItems());
+      }
+    }, [dispatch, products.length, isInitialized]);
+  
+    return { products, loading, error };
+  }
